adb44eea910c9470b1e855bde764f0c0fa893bd6
Divided <woojciechczop@gmail.com>
2020-01-30 23:13:28 +0100

    refactor

--- createVideo.py
+++ createVideo.py
@@ -1,19 +1,16 @@
 import cv2
 
-img_array = []
 path = "C://Users//Divided//Desktop//klatki//"
+filename = "C://Users//Divided//Desktop//klatki//1.jpg"
+img = cv2.imread(filename)
+height, width, layers = img.shape
+size = (width, height)
+out = cv2.VideoWriter(path + 'test.avi', cv2.VideoWriter_fourcc(*'DIVX'), 30, size)
 
 for count in range(len(cv2.os.listdir(path))):
-    filename = "C://Users//Divided//Desktop//klatki//" +str(count) + '.jpg'
+    filename = "C://Users//Divided//Desktop//klatki//" + str(count) + '.jpg'
     img = cv2.imread(filename)
-    height, width, layers = img.shape
-    size = (width,height)
-    img_array.append(img)
+    out.write(img)
     print("appended: " + str(count) + " out of " + str(len(cv2.os.listdir(path))))
-
-out = cv2.VideoWriter(path + 'test.avi',cv2.VideoWriter_fourcc(*'DIVX'), 30, size)
-for i in range(len(img_array)):
-    out.write(img_array[i])
-    print(str(i) + '/' + str(range(len(img_array))))
 
 out.release()
--- run.py
+++ run.py
@@ -6,24 +6,23 @@ import tensornets as nets
 
 from non_max_suppression import non_max_suppression_fast
 from sort import *
-from utils import resize_box, filter_box_by_size
+from utils import resize_box
 
 resizedX, resizedY = 416, 416
 confidance = .4
 inputs = tf.placeholder(tf.float32, [None, resizedX, resizedY, 3])
-model = nets.YOLOv3COCO(inputs, nets.Darknet19)
+model = nets.YOLOv3COCO(inputs, nets.YOLOv3COCO)
 frameCounter = 0
 
 # to display other detected #objects,change the classes and list of classes to their respective #COCO indices available in their website. Here 0th index is for #people and 1 for bicycle and so on. If you want to detect all the #classes, add the indices to this list
 classes = {'1': 'bicycle', '2': 'car', '3': 'motorcycle', '5': 'bus', '7': 'truck'}
-# colors = [(255, 0, 0), (0, 255, 0), (0, 0, 255)]
 list_of_classes = [1, 2, 3, 5, 7]
 
 mot_tracker = Sort(max_age=30, min_hits=1, max_history=300)
 with tf.Session() as sess:
     sess.run(model.pretrained())
 
-    cap = cv2.VideoCapture("C://Users//Divided//Desktop//traffic_test_1.mp4")
+    cap = cv2.VideoCapture("C://Users//Divided//Desktop//test//5.mp4")
     # change the path to your directory or to '0' for webcam
     while cap.isOpened():
         ret, frame = cap.read()
@@ -60,53 +59,58 @@ with tf.Session() as sess:
                         detections.append(detection)
             print(lab, ": ", count)
 
-        filtered = filter_box_by_size(np.array(detections), minWidth=10, minHeight=10, minArea=150)
-        suppressedDetections = non_max_suppression_fast(filtered, 0.4)
-        mot_tracker.update(suppressedDetections)
+        suppressedDetections = non_max_suppression_fast(np.array(detections), 0.4)
+        # filtered = filter_box_by_size(suppressedDetections, minWidth=30, minHeight=30, minArea=400)
+
+        mot_tracker.update(np.array(detections))
         trackerObjects = mot_tracker.trackers
         for t in trackerObjects:
-            if len(t.centroidHistory) > 0:
-                cv2.rectangle(frame, (int(t.history[-1][0][0]), int(t.history[-1][0][1])),
-                              (int(t.history[-1][0][2]), int(t.history[-1][0][1] - 10)), t.color, -1)
+            if t.hit_streak > 1:
+                if len(t.centroidHistory) > 0:
+                    cv2.rectangle(frame, (int(t.history[-1][0][0]), int(t.history[-1][0][1])),
+                                  (int(t.history[-1][0][2]), int(t.history[-1][0][1] - 10)), t.color, -1)
 
-                cv2.putText(frame, str(t.id), (int(t.history[-1][0][0]), int(t.history[-1][0][1] - 1)),
-                            cv2.FONT_HERSHEY_DUPLEX, 0.3,
-                            (255, 255, 255),
-                            lineType=cv2.LINE_AA)
+                    cv2.putText(frame, str(t.id), (int(t.history[-1][0][0]), int(t.history[-1][0][1] - 1)),
+                                cv2.FONT_HERSHEY_DUPLEX, 0.3,
+                                (255, 255, 255),
+                                lineType=cv2.LINE_AA)
 
-                cv2.polylines(frame, [np.asarray(t.centroidHistory).astype(int).reshape((-1, 1, 2))
-                                      ], False, t.color, 1, cv2.LINE_AA)
+                    cv2.polylines(frame, [np.asarray(t.centroidHistory).astype(int).reshape((-1, 1, 2))
+                                          ], False, t.color, 1, cv2.LINE_AA)
 
-                if len(t.centroidHistory) > 1:
-                    cv2.arrowedLine(frame,
-                                    (int(t.centroidHistory[-2][0]), int(t.centroidHistory[-2][1])),
-                                    (int(t.centroidHistory[-1][0]), int(t.centroidHistory[-1][1])),
-                                    t.color, 1, cv2.LINE_AA, 0, 1)
+                    if len(t.centroidHistory) > 1:
+                        cv2.arrowedLine(frame,
+                                        (int(t.centroidHistory[-2][0]), int(t.centroidHistory[-2][1])),
+                                        (int(t.centroidHistory[-1][0]), int(t.centroidHistory[-1][1])),
+                                        t.color, 1, cv2.LINE_AA, 0, 1)
 
-            if len(t.history) > 0:
-                cv2.rectangle(frame, (int(t.history[-1][0][0]), int(t.history[-1][0][1])),
-                              (int(t.history[-1][0][2]), int(t.history[-1][0][3])), t.color, 1)
+                if len(t.history) > 0:
+                    cv2.rectangle(frame, (int(t.history[-1][0][0]), int(t.history[-1][0][1])),
+                                  (int(t.history[-1][0][2]), int(t.history[-1][0][3])), t.color, 1)
 
-                cv2.putText(frame, "{:.2f}".format(t.confidence),
-                            (int(t.history[-1][0][0]), int(t.history[-1][0][1] + 10)),
-                            cv2.FONT_HERSHEY_DUPLEX, 0.3,
-                            (255, 255, 255),
-                            lineType=cv2.LINE_AA)
+                    cv2.putText(frame, "{:.2f}".format(t.confidence),
+                                (int(t.history[-1][0][0]), int(t.history[-1][0][1] + 10)),
+                                cv2.FONT_HERSHEY_DUPLEX, 0.3,
+                                (255, 255, 255),
+                                lineType=cv2.LINE_AA)
 
-                cv2.rectangle(frame, (int(t.history[-1][0][2]) - 10, int(t.history[-1][0][1])),
-                              (int(t.history[-1][0][2]), int(t.history[-1][0][1] + 10)), t.color, 1)
+                    cv2.rectangle(frame, (int(t.history[-1][0][2]) - 10, int(t.history[-1][0][1])),
+                                  (int(t.history[-1][0][2]), int(t.history[-1][0][1] + 10)), t.color, 1)
 
-                cv2.putText(frame, classes[str(int(t.predicted_class))][0].upper(),
-                            (int(t.history[-1][0][2]) - 8, int(t.history[-1][0][1] + 9)),
-                            cv2.FONT_HERSHEY_DUPLEX, 0.3,
-                            (255, 255, 255),
-                            lineType=cv2.LINE_AA)
+                    cv2.putText(frame, classes[str(int(t.predicted_class))][0].upper(),
+                                (int(t.history[-1][0][2]) - 8, int(t.history[-1][0][1] + 9)),
+                                cv2.FONT_HERSHEY_DUPLEX, 0.3,
+                                (255, 255, 255),
+                                lineType=cv2.LINE_AA)
 
         computeTime = (time.time() - start_time)
         fps = 1 / computeTime
-        print("Time: " + "{:3.f}".format(computeTime))
+        print("Time: " + str(int(computeTime * 1000)))
         print("FPS: " + "{:.2f}".format(fps))  # to time it
-        cv2.putText(frame,str(frameWidth)+"x"+str(frameHeight) + " " + "{:3.}".format(computeTime) + " ms " + "{:.2f}".format(fps) + " fps" + " frame " + str(frameCounter), (0, 20),
+        cv2.putText(frame, str(frameWidth) + "x" + str(frameHeight) + " " + str(
+            int(computeTime * 1000)) + " ms " + "{:.2f}".format(
+            fps) + " fps" + " frame " + str(
+            frameCounter), (0, 20),
                     cv2.FONT_HERSHEY_SIMPLEX, 0.7,
                     (255, 255, 255),
                     lineType=cv2.LINE_AA)
